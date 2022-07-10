c60bc9bcad6333c56eda7563b6adfcb8a3b28039
leszczynskimaciek98 <maciek.leszcz1998@gmail.com>
2022-07-10 23:48:47 +0200

    Dodano interakcje z wpisywanymi wartościami w panelu SignUpActivity

--- app/src/main/java/com/example/kettle_app_test/activities/MainActivity.java
+++ app/src/main/java/com/example/kettle_app_test/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.kettle_app_test;
+package com.example.kettle_app_test.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.kettle_app_test.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/kettle_app_test/activities/SignUpActivity.java
+++ app/src/main/java/com/example/kettle_app_test/activities/SignUpActivity.java
@@ -3,14 +3,63 @@ package com.example.kettle_app_test.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Toast;
 
 import com.example.kettle_app_test.R;
+import com.example.kettle_app_test.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+    private String encodedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+        binding.buttonSignUp.setOnClickListener(v ->{
+            if(isValidSignUpDetails()){
+                signUp();
+            }
+        });
+    }
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    private void signUp(){
+
+    }
+    private Boolean isValidSignUpDetails(){
+        if(encodedImage == null){
+            showToast("Select profile image");
+            return false;
+        }else if(binding.inputName.getText().toString().trim().isEmpty()){
+            showToast("Enter name");
+            return false;
+        }else if(binding.inputEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter email");
+            return false;
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+            showToast("Enter valid image");
+            return false;
+        }else if(binding.inputPassword.getText().toString().trim().isEmpty()){
+            showToast("Enter password");
+            return false;
+        }else if(binding.inputConfirmPassword.getText().toString().trim().isEmpty()){
+            showToast("Confirm password");
+            return false;
+        }else if(!binding.inputPassword.getText().toString().equals(binding.inputConfirmPassword.getText().toString())){
+            showToast("Password & confirm password must be the same");
+            return false;
+        }else{
+            return true;
+        }
     }
 }
